9ada1db12c575d1348a5052f5b5eabd081ac3bed
AlexDzha <s.dzhabarov@mail.ru>
2024-07-09 21:19:36 +0300

    homework commit

--- Модуль 2/2. Домашняя работа по уроку Условная конструкция. Операторы if, elif, else/homework1.py
+++ Модуль 2/2. Домашняя работа по уроку Условная конструкция. Операторы if, elif, else/homework1.py
@@ -1,9 +1,9 @@
 first = int(input('Введите число:  '))
 second = int(input('Введите число:  '))
 third = int(input('Введите число:  '))
-if first == second == third:
-    print('Кол-во одинаковых чисел: 3')
+if first == second and second == third:
+    print(3)
 elif first == second or second == third or first == third:
-    print('Кол-во одинаковых чисел: 2')
+    print(2)
 else:
-    print('Кол-во одинаковых чисел: 0')
+    print(0)
