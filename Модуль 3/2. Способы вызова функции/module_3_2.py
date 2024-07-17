df18177091966adbbfde55bfc6071411b45c82ee
AlexDzha <s.dzhabarov@mail.ru>
2024-07-17 22:39:35 +0300

    homework commit

--- Модуль 3/2. Способы вызова функции/module_3_2.py
+++ Модуль 3/2. Способы вызова функции/module_3_2.py
@@ -3,10 +3,10 @@ def send_email(message,recipient,*,sender = "university.help@gmail.com"):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
     elif "@" not in sender or (".com" not in sender and ".ru" not in sender and "net" not in sender):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-    elif sender == recipient:(
-        print("Нельзя отправить письмо самому себе!"))
-    elif sender == "university.help@gmail.com":(
-        print(f"Письмо успешно отправлено с адреса{sender} на адрес {recipient}."))
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса{sender} на адрес {recipient}.")
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
