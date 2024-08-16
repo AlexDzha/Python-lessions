d800b5fa93a6b9ec927542f9b55f230d07c0a108
AlexDzha <s.dzhabarov@mail.ru>
2024-08-16 11:32:49 +0300

    Commit

--- Модуль 5/1. Атрибуты и методы объекта/module_5_1.py
+++ Модуль 5/1. Атрибуты и методы объекта/module_5_1.py
@@ -1,24 +1,27 @@
 
-class House:
+class House: #создали класс
 
-    def __init__(self, name, number_of_floors):
+    def __init__(self, name, number_of_floors): #добавили атрибуты
         self.name = name
         self.number_of_floors = number_of_floors
 
 
-    def go_to(self, new_floor):
-        if new_floor>self.number_of_floors or new_floor<1:
+    def go_to(self, new_floor): #создаем метод go_to с параметром new_floor
+        if new_floor>self.number_of_floors or new_floor<1: #условие для несуществующего этажа
             print('"Такого этажа не существует"')
+    # закоментированный (работающий) вариант вместо or отдельным условием
         # elif new_floor < 1:
         #      print('"Такого этажа не существует"')
-        else:
-             for i in range(1, new_floor + 1):
+        else: #условие для существующих этажей
+             for i in range(1, new_floor + 1): # выводит значение начиная с 1 этажа заканчивая нужным этажом
                  print(i)
 
 
 
-
+# Исходные данные: (можно добавить еще при желании)
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
+# h3 = House('Родовое поместье', 3)
 h1.go_to(5)
-h2.go_to(-7)
+h2.go_to(10)
+# h3.go_to(0)
