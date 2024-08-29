061b1e2ac57d168320cfec9b30cdab5f01384570
AlexDzha <s.dzhabarov@mail.ru>
2024-08-29 13:44:09 +0300

    Commit

--- Модуль 5/Практика. Система регистрации на классах/practicе.py
+++ Модуль 5/Практика. Система регистрации на классах/practicе.py
@@ -13,6 +13,7 @@ class Database:
         self.data[username] = password
 class User:
     """
+
     Класс пользователя, содержащий атрибуты: логин и пароль
     """
     def __init__(self, username, password, password_confirm):
@@ -20,32 +21,41 @@ class User:
         if password == password_confirm:
             self.password = password
 
-
-
-
 if __name__ == '__main__':
     database = Database()
     while True:
-        choice = input("Приветствую! Выберите действие: \n1 - вход\n2 - Регистрация\n")
-        user = User(input("Введите логин: "), password := input("Введите пароль: "), password2 := input("Повторите пароль: "))
-        if len(password) <8 or len(password) >20:
-            print(bad_password)
-            exit()
-        elif not any(char.isdigit() for char in password):
-            print(bad_password)
-            exit()
-        elif not any(char.isupper() for char in password):
-            print(bad_password)
-            exit()
-        elif not any(char.islower() for char in password):
-            print(bad_password)
-            exit()
-        elif not any(char in Special_Symbols for char in password):
-            print(bad_password)
-            exit()
-        elif password != password2:
-            print('Введенные пароли не совпадают')
-            exit()
+        choice = int(input("Приветствую! Выберите действие: \n1 - вход\n2 - Регистрация\n"))
+        if choice == 1:
+            login = input("Введите логин: ")
+            password = input("Введите пароль: ")
+            if login in database.data:
+                if password == database.data[login]:
+                    print(f'Вход выполнен, {login}')
+                else:
+                    print('Неверный пароль')
+            else:
+                print("Пользователь не найден.")
+        if choice == 2:
+            user = User(input("Введите логин: "), password := input("Введите пароль: "),
+                        password2 := input("Повторите пароль: "))
+            if len(password) <8 or len(password) >20:
+                print(bad_password)
+                continue
+            elif not any(char.isdigit() for char in password):
+                print(bad_password)
+                continue
+            elif not any(char.isupper() for char in password):
+                print(bad_password)
+                continue
+            elif not any(char.islower() for char in password):
+                print(bad_password)
+                continue
+            elif not any(char in Special_Symbols for char in password):
+                print(bad_password)
+                continue
+            elif password != password2:
+                print('Введенные пароли не совпадают, попробуйте еще раз.')
+                continue
 
-        database.add_user(user.username, user.password)
+            database.add_user(user.username, user.password)
         print(database.data)
