a7dd257811ab52186badf13fa13401f19bb8c7b7
AlexDzha <s.dzhabarov@mail.ru>
2024-09-12 11:06:07 +0300

    Commit

--- Модуль 6/2. Доступ к свойствам родителя. Переопределение свойств/module_6_2.py
+++ Модуль 6/2. Доступ к свойствам родителя. Переопределение свойств/module_6_2.py
@@ -1,12 +1,12 @@
 class Vehicle:
-
+    __COLOR_VARIANTS = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'white', 'black']
     def __init__(self, owner, __model, __color, __engine_power):
         self.owner: str = owner # владелец транспорта. (изм.)
         self.__model: str = __model # модель (марка) транспорта. (неизм.)
         self.__engine_power: int = __engine_power # мощность двигателя. (неизм.)
         self.__color: str = __color # название цвета. (неизм.)
 
-    __COLOR_VARIANTS = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'white', 'black']
+
     def get_model(self):
         return f"Модель: {self.__model}"
     def get_horsepower(self):
